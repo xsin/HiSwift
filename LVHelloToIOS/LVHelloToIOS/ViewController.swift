20978b3003c184b6478fedd7052e83a66a8be367
mamboer <mamboer@live.com>
2014-09-25 12:56:11 +0800

    gg

---
+++ LVHelloToIOS/LVHelloToIOS/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  LVHelloToIOS
+//
+//  Created by Levin Wong on 9/24/14.
+//  Copyright (c) 2014 Levin Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        searchItunesFor("JQ Software")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        cell.textLabel?.text = rowData["trackName"] as? String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        var urlString: NSString = rowData["artworkUrl60"] as NSString
+        var imgURL: NSURL = NSURL(string: urlString)
+        
+        // Download an NSData representation of the image at the URL
+        var imgData: NSData = NSData(contentsOfURL: imgURL)
+        cell.imageView?.image = UIImage(data: imgData)
+        
+        // Get the formatted price string for display in the subtitle
+        var formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        cell.detailTextLabel?.text = formattedPrice
+        
+        return cell
+    }
+    
+    @IBOutlet var appsTableView : UITableView?
+    var data: NSMutableData = NSMutableData()
+    var tableData: NSArray = NSArray()
+    
+    func searchItunesFor(searchTerm: String) {
+        
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        var escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        var urlPath = "https://itunes.apple.com/search?term=\(escapedSearchTerm!)&media=software"
+        var url: NSURL = NSURL(string: urlPath)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)
+        
+        println("Search iTunes API at URL \(urlPath)")
+        
+        connection.start()
+    }
+    
+    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+        // Recieved a new request, clear out the data object
+        self.data = NSMutableData()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        // Append the recieved chunk of data to our data object
+        self.data.appendData(data)
+    }
+    
+    func connection(connection: NSURLConnection!, didFailWithError error: NSError!) {
+        println("Connection failed.\(error.localizedDescription)")
+    }
+    
+    func connection(connection: NSURLConnection, didRecieveResponse response: NSURLResponse)  {
+        println("Recieved response")
+    }
+    
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        // Request complete, self.data should now hold the resulting info
+        // Convert the retrieved data in to an object through JSON deserialization
+        var err: NSError
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options:    NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        
+        if jsonResult.count>0 && jsonResult["resultCount"] as Int > 0 {
+            var results: NSArray = jsonResult["results"] as NSArray
+            self.tableData = results
+            self.appsTableView?.reloadData()
+            
+        }
+    }
+
+}
+
+
